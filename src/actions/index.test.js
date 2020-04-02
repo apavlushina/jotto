4184fe9fc70ace178c030d3facc20856d9c60ba2
apavlushina <ssemina7@gmail.com>
2020-04-02 14:49:08 +0200

    setup guessedWord tests

--- src/actions/index.test.js
+++
@@ -1,9 +0,0 @@
-import { correctGuess, actionTypes } from "./index";
-import { exportAllDeclaration } from "babel-types";
-
-describe("correctGuess", () => {
-  test("returns an action with type CORRECT_GUESS", () => {
-    const action = correctGuess();
-    expect(action).toEqual({ type: actionTypes.CORRECT_GUESS });
-  });
-});
---
+++ src/integration.test.js
@@ -0,0 +1,13 @@
+import { storeFactory } from "../tests/testUnits";
+import { guessWord } from "./actions";
+
+describe("guessWord action dispatcher", () => {
+  describe("no guessed words", () => {
+    test("updates state correctlly for unsuccessful guess", () => {});
+    test("updates state correctlly for successful guess", () => {});
+  });
+  describe("some guessed words", () => {
+    test("updates state correctlly for unsuccessful guess", () => {});
+    test("updates state correctlly for successful guess", () => {});
+  });
+});
