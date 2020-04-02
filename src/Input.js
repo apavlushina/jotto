3823f07a77a7e041fa41f1651f418ad5d8b34c3e
apavlushina <ssemina7@gmail.com>
2020-04-02 13:23:07 +0200

    complete Input component render tests for success=false

---
+++ src/Input.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { connect } from "react-redux";
+
+class Input extends React.PureComponent {
+  render() {
+    const contents = this.props.success ? null : (
+      <form className="form-inline">
+        <input
+          data-test="input-box"
+          type="text"
+          className="mb-2 mx-sm-3"
+          placeholder="enter guess"
+        ></input>
+        <button
+          data-test="submit-button"
+          type="submit"
+          className="btn btn-primary mb-2"
+        >
+          Submit
+        </button>
+      </form>
+    );
+    return <div data-test="component-input">{contents}</div>;
+  }
+}
+
+const mapStateToProps = state => {
+  return {};
+};
+
+export default connect(mapStateToProps)(Input);
